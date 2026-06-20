2ba50043280dc3c683764627bc3925c95556019a
MaduGoncalves <mariaeduh7@gmail.com>
2026-06-20 15:36:32 -0400

    test: adiciona testes para shortValue e intValue

--- tests/test_jinteger.py
+++ tests/test_jinteger.py
@@ -23,3 +23,14 @@ class TestByteValue:
 
     def test_negativo_129_estoura_para_127(self):
         assert JInteger(-129).byteValue() == 127
+
+class TestShortValue:
+    def test_dentro_do_intervalo_signed_short(self):
+        assert JInteger(1000).shortValue() == 1000
+
+    def test_32768_estoura_para_negativo_32768(self):
+        assert JInteger(32768).shortValue() == -32768
+
+class TestIntValue:
+    def test_retorna_valor_original(self):
+        assert JInteger(12345).intValue() == 12345
