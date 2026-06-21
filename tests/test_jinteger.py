28c8eae54b19fec23dc9c4b3bc014e53391af36d
MaduGoncalves <mariaeduh7@gmail.com>
2026-06-21 14:25:43 -0400

    test: adiciona teste para float value e to binary string

--- tests/test_jinteger.py
+++ tests/test_jinteger.py
@@ -48,3 +48,13 @@ class TestLongValue:
 class TestFloatValue:
     def test_conversao_valor_simples(self):
         assert JInteger(42).floatValue() == 42.0
+
+    def test_conversao_valor_negativo(self):
+        assert JInteger(-42).floatValue() == -42.0
+        
+class TestToBinaryString:
+    def test_valor_positivo_simples(self):
+        assert JInteger.toBinaryString(4) == '100'
+ 
+    def test_negativo_interpretado_como_unsigned_32_bits(self):
+        assert JInteger.toBinaryString(-1) == '1' * 32
