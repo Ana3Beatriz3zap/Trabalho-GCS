475cd35bfde2822b8fcccc04a58c226ab3a78aa2
MaduGoncalves <mariaeduh7@gmail.com>
2026-06-21 15:11:44 -0400

    test: adiciona testes para compareUnsigned e divideUnsigned

--- tests/test_jinteger.py
+++ tests/test_jinteger.py
@@ -48,3 +48,15 @@ class TestLongValue:
 class TestFloatValue:
     def test_conversao_valor_simples(self):
         assert JInteger(42).floatValue() == 42.0
+
+class TestCompareUnsigned:
+    def test_menos_um_e_maior_que_qualquer_positivo_como_unsigned(self):
+        assert JInteger.compareUnsigned(-1, 1) > 0
+ 
+    def test_valores_iguais_retorna_zero(self):
+        assert JInteger.compareUnsigned(-1, -1) == 0
+
+class TestDivideUnsigned:
+    def test_divisao_simples(self):
+        assert JInteger.divideUnsigned(10, 3) == 3
+ 
